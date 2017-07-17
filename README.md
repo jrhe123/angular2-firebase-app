38025f0dc35905f1f4d3927d62a5c5034ac03d3c
royhe <roy@poprx.ca>
2017-07-16 21:04:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Angular2-firebase app demo
+
+1. Google Authentication
+2. Angular2
+3. Firebase
+4. Image Uploads
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { FlashMessagesService } from 'angular2-flash-messages';
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    public af:AngularFire,
+    public flashMessage:FlashMessagesService
+    ) { }
+
+  // Collapse settings
+  isIn = false;
+  toggleState() {
+    let bool = this.isIn;
+    this.isIn = bool === false ? true : false; 
+  }
+
+  ngOnInit() {
+  }
+
+  login(){
+  	this.af.auth.login();
+  }
+
+  logout(){
+  	this.af.auth.logout();
+    this.flashMessage.show('You are logout', {cssClass:'alert-success',timeout: 3000});
+  }
+
+}
